09dea3eecb86f602bf8f3560b590a5254a648c73
XiaoKitty <XiaoKitty-adbUSB@github.com-all>
2026-04-28 05:17:43 +0800

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
         debug {
             isMinifyEnabled = false
             isDebuggable = true
-            signingConfig = signingConfigs.getByName("adb")
+         //   signingConfig = signingConfigs.getByName("adb")
         }
     }
 
