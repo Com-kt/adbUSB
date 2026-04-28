67b7de76a2aae967dcc924313e51cbb6ac518027
XiaoKitty <XiaoKitty-adbUSB@github.com-all>
2026-04-28 23:51:44 +0800

    Update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,31 @@
+rootProject.name = "adbUSB"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
-  repositories {
-    gradlePluginPortal()
-    google()
-    mavenCentral()
-  }
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-    gradlePluginPortal()
-    maven("https://jitpack.io")
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
-    maven("https://maven.scijava.org/content/repositories/public/")
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://s01.oss.sonatype.org/content/groups/public/")
-    maven("https://plugins.gradle.org/m2/")
-  }
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
 }
-
-rootProject.name = "adbUSB"
 
 include(":app")
