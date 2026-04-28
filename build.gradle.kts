426f8e9548824378484668d08d7e3b7eb12a092d
XiaoKitty <XiaoKitty-adbUSB@github.com-all>
2026-04-29 00:48:20 +0800

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     val gradle_version = "9.2.0"
-  //  val kotlin_version = "2.3.21"
+    val kotlin_version = "2.3.21"
     id("com.android.application") version "$gradle_version" apply false
     id("com.android.library") version "$gradle_version" apply false
-   // id("org.jetbrains.kotlin.android") version "$kotlin_version" apply false
+    id("org.jetbrains.kotlin.android") version "$kotlin_version" apply false
 }
 
 tasks.register<Delete>("clean") {
