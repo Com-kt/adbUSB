21c2e58f31b5f37d70b6cbcb8b45462d8fd80a6c
XiaoKitty <XiaoKitty-usbFlash@github.com-all>
2026-06-22 21:23:09 +0800

    Update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -34,7 +34,11 @@ Java_com_adb_kitty_compose_data_NativeLibs_UserString(JNIEnv *env, jobject) {
     return env->NewStringUTF(USER_KITTY_CONSTANT);
 }
 
-#define HARD_ASSERT(ptr, exit_code) if (!(ptr)) { std::exit(exit_code); }
+#define HARD_ASSERT(env, ptr, exit_code) \
+    if (!(ptr)) { \
+        if ((env)->ExceptionCheck()) { (env)->ExceptionClear(); } \
+        std::exit(exit_code); \
+    }
 
 const uint8_t EXPECTED_SHA256[32] = {
     0x92, 0x6f, 0x6e, 0x03, 0x60, 0xe3, 0xe8, 0xf6,
@@ -100,15 +104,15 @@ Java_com_adb_kitty_compose_data_NativeLibs_nativeVerify(JNIEnv *env, jobject thi
     }
 
     jclass contextClz = env->GetObjectClass(context);
-    HARD_ASSERT(contextClz, 100);
+    HARD_ASSERT(env, contextClz, 100);
     
     jmethodID getPackageCodePathMd = env->GetMethodID(contextClz, 
         _XOR_({ 0x3d, 0x3f, 0x2e, 0x0a, 0x3b, 0x39, 0x31, 0x3b, 0x3d, 0x3f, 0x19, 0x35, 0x3e, 0x3f, 0x0a, 0x3b, 0x2e, 0x32 }).c_str(), 
         _XOR_({ 0x72, 0x73, 0x16, 0x30, 0x3b, 0x2c, 0x3b, 0x75, 0x36, 0x3b, 0x34, 0x3d, 0x75, 0x09, 0x2e, 0x28, 0x33, 0x34, 0x3d, 0x61 }).c_str());
-    HARD_ASSERT(getPackageCodePathMd, 101);
+    HARD_ASSERT(env, getPackageCodePathMd, 101);
     
     auto jApkPath = (jstring)env->CallObjectMethod(context, getPackageCodePathMd);
-    HARD_ASSERT(jApkPath, 102);
+    HARD_ASSERT(env, jApkPath, 102);
     
     const char* apkPathChars = env->GetStringUTFChars(jApkPath, nullptr);
     std::string apkPath(apkPathChars);
@@ -121,48 +125,48 @@ Java_com_adb_kitty_compose_data_NativeLibs_nativeVerify(JNIEnv *env, jobject thi
     jmethodID getPackageManagerMd = env->GetMethodID(contextClz, 
         _XOR_({ 0x3d, 0x3f, 0x2e, 0x0a, 0x3b, 0x39, 0x31, 0x3b, 0x3d, 0x3f, 0x17, 0x3b, 0x34, 0x3b, 0x3d, 0x3f, 0x28 }).c_str(), 
         _XOR_({ 0x72, 0x73, 0x16, 0x3b, 0x34, 0x3e, 0x28, 0x35, 0x33, 0x3e, 0x75, 0x39, 0x35, 0x34, 0x2e, 0x3f, 0x34, 0x2e, 0x75, 0x2a, 0x37, 0x75, 0x0a, 0x3b, 0x39, 0x31, 0x3b, 0x3d, 0x3f, 0x17, 0x3b, 0x34, 0x3b, 0x3d, 0x3f, 0x28, 0x61 }).c_str());
-    HARD_ASSERT(getPackageManagerMd, 103);
+    HARD_ASSERT(env, getPackageManagerMd, 103);
     
     jobject packageManager = env->CallObjectMethod(context, getPackageManagerMd);
-    HARD_ASSERT(packageManager, 104);
+    HARD_ASSERT(env, packageManager, 104);
     
     jmethodID getPackageNameMd = env->GetMethodID(contextClz, 
         _XOR_({ 0x3d, 0x3f, 0x2e, 0x0a, 0x3b, 0x39, 0x31, 0x3b, 0x3d, 0x3f, 0x14, 0x3b, 0x37, 0x3f }).c_str(), 
         _XOR_({ 0x72, 0x73, 0x16, 0x30, 0x3b, 0x2c, 0x3b, 0x75, 0x36, 0x3b, 0x34, 0x3d, 0x75, 0x09, 0x2e, 0x28, 0x33, 0x34, 0x3d, 0x61 }).c_str());
-    HARD_ASSERT(getPackageNameMd, 105);
+    HARD_ASSERT(env, getPackageNameMd, 105);
     
     auto packageName = (jstring)env->CallObjectMethod(context, getPackageNameMd);
-    HARD_ASSERT(packageName, 106);
+    HARD_ASSERT(env, packageName, 106);
     
     jclass pmClz = env->GetObjectClass(packageManager);
-    HARD_ASSERT(pmClz, 107);
+    HARD_ASSERT(env, pmClz, 107);
 
     jmethodID getPackageInfoMd = env->GetMethodID(pmClz, 
         _XOR_({ 0x3d, 0x3f, 0x2e, 0x0a, 0x3b, 0x39, 0x31, 0x3b, 0x3d, 0x3f, 0x13, 0x34, 0x3c, 0x35 }).c_str(), 
         _XOR_({ 0x72, 0x16, 0x30, 0x3b, 0x2c, 0x3b, 0x75, 0x36, 0x3b, 0x34, 0x3d, 0x75, 0x09, 0x2e, 0x28, 0x33, 0x34, 0x3d, 0x61, 0x13, 0x73, 0x16, 0x3b, 0x34, 0x3e, 0x28, 0x35, 0x33, 0x3e, 0x75, 0x39, 0x35, 0x34, 0x2e, 0x3f, 0x34, 0x2e, 0x75, 0x2a, 0x37, 0x75, 0x0a, 0x3b, 0x39, 0x31, 0x3b, 0x3d, 0x3f, 0x13, 0x34, 0x3c, 0x35, 0x61 }).c_str());
-    HARD_ASSERT(getPackageInfoMd, 108);
+    HARD_ASSERT(env, getPackageInfoMd, 108);
     
     jobject packageInfo = env->CallObjectMethod(packageManager, getPackageInfoMd, packageName, 0x08000000);
-    HARD_ASSERT(packageInfo, 109);
+    HARD_ASSERT(env, packageInfo, 109);
     
     jclass packageInfoClz = env->GetObjectClass(packageInfo);
-    HARD_ASSERT(packageInfoClz, 110);
+    HARD_ASSERT(env, packageInfoClz, 110);
     
     jfieldID signingInfoFd = env->GetFieldID(packageInfoClz, 
         _XOR_({ 0x29, 0x33, 0x3d, 0x34, 0x33, 0x34, 0x3d, 0x13, 0x34, 0x3c, 0x35 }).c_str(), 
         _XOR_({ 0x16, 0x3b, 0x34, 0x3e, 0x28, 0x35, 0x33, 0x3e, 0x75, 0x39, 0x35, 0x34, 0x2e, 0x3f, 0x34, 0x2e, 0x75, 0x2a, 0x37, 0x75, 0x09, 0x33, 0x3d, 0x34, 0x33, 0x34, 0x3d, 0x13, 0x34, 0x3c, 0x35, 0x61 }).c_str());
-    HARD_ASSERT(signingInfoFd, 111);
+    HARD_ASSERT(env, signingInfoFd, 111);
     
     jobject signingInfo = env->GetObjectField(packageInfo, signingInfoFd);
-    HARD_ASSERT(signingInfo, 112);
+    HARD_ASSERT(env, signingInfo, 112);
     
     jclass signingInfoClz = env->GetObjectClass(signingInfo);
-    HARD_ASSERT(signingInfoClz, 113);
+    HARD_ASSERT(env, signingInfoClz, 113);
     
     jmethodID getApkContentsSignersMd = env->GetMethodID(signingInfoClz, 
         _XOR_({ 0x3d, 0x3f, 0x2e, 0x1b, 0x2a, 0x31, 0x19, 0x35, 0x34, 0x2e, 0x3f, 0x34, 0x2e, 0x29, 0x09, 0x33, 0x3d, 0x34, 0x3f, 0x28, 0x29 }).c_str(), 
         _XOR_({ 0x72, 0x73, 0x01, 0x16, 0x3b, 0x34, 0x3e, 0x28, 0x35, 0x33, 0x3e, 0x75, 0x39, 0x35, 0x34, 0x2e, 0x3f, 0x34, 0x2e, 0x75, 0x2a, 0x37, 0x75, 0x09, 0x33, 0x3d, 0x34, 0x3b, 0x2e, 0x2f, 0x28, 0x3f, 0x61 }).c_str());
-    HARD_ASSERT(getApkContentsSignersMd, 114);
+    HARD_ASSERT(env, getApkContentsSignersMd, 114);
     
     auto signatureArray = (jobjectArray)env->CallObjectMethod(signingInfo, getApkContentsSignersMd);
     
@@ -170,43 +174,49 @@ Java_com_adb_kitty_compose_data_NativeLibs_nativeVerify(JNIEnv *env, jobject thi
     if (signatureArray && env->GetArrayLength(signatureArray) > 0) {
         signatureObj = env->GetObjectArrayElement(signatureArray, 0);
     }
-    HARD_ASSERT(signatureObj, 115);
+    HARD_ASSERT(env, signatureObj, 115);
 
     jclass signatureClz = env->GetObjectClass(signatureObj);
-    HARD_ASSERT(signatureClz, 116);
+    HARD_ASSERT(env, signatureClz, 116);
     
     jmethodID toByteArrayMd = env->GetMethodID(signatureClz, 
         _XOR_({ 0x2e, 0x35, 0x18, 0x23, 0x2e, 0x3f, 0x1b, 0x28, 0x28, 0x3b, 0x23 }).c_str(), 
         _XOR_({ 0x72, 0x73, 0x01, 0x18 }).c_str());
-    HARD_ASSERT(toByteArrayMd, 117);
+    HARD_ASSERT(env, toByteArrayMd, 117);
     
     auto certificateBytes = (jbyteArray)env->CallObjectMethod(signatureObj, toByteArrayMd);
-    HARD_ASSERT(certificateBytes, 118);
+    HARD_ASSERT(env, certificateBytes, 118);
 
     jclass messageDigestClz = env->FindClass(_XOR_({ 0x30, 0x3b, 0x2c, 0x3b, 0x75, 0x36, 0x3b, 0x34, 0x3d, 0x75, 0x17, 0x3f, 0x29, 0x29, 0x3b, 0x3d, 0x3f, 0x1e, 0x33, 0x3d, 0x3f, 0x29, 0x2e }).c_str());
-    HARD_ASSERT(messageDigestClz, 119);
+    HARD_ASSERT(env, messageDigestClz, 119);
     
     jmethodID getInstanceMd = env->GetStaticMethodID(messageDigestClz, 
         _XOR_({ 0x3d, 0x3f, 0x2e, 0x13, 0x34, 0x29, 0x2e, 0x3b, 0x34, 0x39, 0x3f }).c_str(), 
         _XOR_({ 0x72, 0x16, 0x30, 0x3b, 0x2c, 0x3b, 0x75, 0x36, 0x3b, 0x34, 0x3d, 0x75, 0x09, 0x2e, 0x28, 0x33, 0x34, 0x3d, 0x61, 0x73, 0x16, 0x30, 0x3b, 0x2c, 0x3b, 0x75, 0x36, 0x3b, 0x34, 0x3d, 0x75, 0x17, 0x3f, 0x29, 0x29, 0x3b, 0x3d, 0x3f, 0x1e, 0x33, 0x3d, 0x3f, 0x29, 0x2e, 0x61 }).c_str());
-    HARD_ASSERT(getInstanceMd, 120);
+    HARD_ASSERT(env, getInstanceMd, 120);
     
-    jobject messageDigest = env->CallStaticObjectMethod(messageDigestClz, getInstanceMd, env->NewStringUTF(_XOR_({ 0x09, 0x12, 0x1b, 0x77, 0x68, 0x6f, 0x6c }).c_str()));
-    HARD_ASSERT(messageDigest, 121);
+    jstring algoNameStr = env->NewStringUTF(_XOR_({ 0x09, 0x12, 0x1b, 0x77, 0x68, 0x6f, 0x6c }).c_str());
+    jobject messageDigest = env->CallStaticObjectMethod(messageDigestClz, getInstanceMd, algoNameStr);
+    env->DeleteLocalRef(algoNameStr);
+    HARD_ASSERT(env, messageDigest, 121);
     
     jmethodID digestMd = env->GetMethodID(messageDigestClz, 
         _XOR_({ 0x3e, 0x33, 0x3d, 0x3f, 0x29, 0x2e }).c_str(), 
-        _XOR_({ 0x72, 0x01, 0x18, 0x73, 0x01, 0x18 }).c_str());
-    HARD_ASSERT(digestMd, 122);
+        _XOR_({ 0x28, 0x5b, 0x42, 0x29, 0x5b, 0x42 }).c_str());
+    HARD_ASSERT(env, digestMd, 122);
     
     auto currentHashBytes = (jbyteArray)env->CallObjectMethod(messageDigest, digestMd, certificateBytes);
-    HARD_ASSERT(currentHashBytes, 123);
+    HARD_ASSERT(env, currentHashBytes, 123);
+
+    jsize shaLen = env->GetArrayLength(currentHashBytes);
+    if (shaLen != 32) { std::exit(402); }
 
     jbyte* hashes = env->GetByteArrayElements(currentHashBytes, nullptr);
+    HARD_ASSERT(env, hashes, 124);
 
     bool match = true;
     for (int i = 0; i < 32; ++i) {
-        if (((uint8_t)hashes[i]) != EXPECTED_SHA256[i]) {
+        if ((static_cast<uint8_t>(hashes[i])) != EXPECTED_SHA256[i]) {
             match = false;
             break;
         }
