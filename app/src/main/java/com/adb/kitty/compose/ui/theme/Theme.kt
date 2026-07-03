ce9fc07ab7f79dcf671a7b0f43f333a8c4324047
SmallNeko <SmallNeko@github.com-all>
2026-07-04 02:57:35 +0800

    Update

--- app/src/main/java/com/adb/kitty/compose/ui/theme/Theme.kt
+++ app/src/main/java/com/adb/kitty/compose/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
@@ -27,6 +28,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Keep
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun ComposeEmptyActivityTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
