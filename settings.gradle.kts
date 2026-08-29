a75eda9cbc3cdd1ed921fbeca5951ae51d758e1d
neko <neko@github.com-all>
2026-08-29 17:32:39 +0800

    Update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,8 +19,6 @@ dependencyResolutionManagement {
     maven("https://jitpack.io")
     maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
     maven("https://maven.scijava.org/content/repositories/public/")
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://s01.oss.sonatype.org/content/groups/public/")
     maven("https://plugins.gradle.org/m2/")
     maven("https://redirector.kotlinlang.org/maven/bootstrap")
   }
