8b1c158f7864767200220bcc98cc9a3e25eadfcc
neko <neko@github.com-all>
2026-08-29 17:27:13 +0800

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -175,6 +175,7 @@ dependencies {
     runtimeOnly(libs.bundles.coroutines.runtime)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.annotation)
+    implementation(libs.androidx.recyclerview)
     implementation(libs.lsposed.hiddenapibypass)
     implementation(libs.nayuki.qrcode)
     implementation(libs.zxing.core)
